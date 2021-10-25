ad6de98dfb491f00d1bc5f3f9d3e3fa6f4e415b1
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-10-25 18:15:56 +0100

    initial commit

---
+++ tutors/src/test/kotlin/com/artemas/tutors/TutorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemas.tutors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutorsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
