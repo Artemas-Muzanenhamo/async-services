ca98584f87c11b2ef1c511a30ec19ce5f6a9dbc0
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-11-23 21:26:11 +0000

    clean up code

--- university/src/main/kotlin/com/artemas/university/web/UniversityEndpoint.kt
+++ university/src/main/kotlin/com/artemas/university/web/UniversityEndpoint.kt
@@ -6,27 +6,32 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClient.create
 import reactor.core.publisher.Flux
 import reactor.util.function.Tuple2
 
 @RestController
 @RequestMapping("/api")
-class UniversityEndpoint(
-    val webClient: WebClient = WebClient.create()
-) {
+class UniversityEndpoint(val webClient: WebClient = create()) {
+
     @GetMapping("/university")
     fun getAllStudentsAndTutors(): Flux<Tuple2<Student, Tutor>> {
         return webClient.get()
-            .uri("http://localhost:8082/api/students")
+            .uri(STUDENTS_API)
             .retrieve()
             .bodyToFlux(Student::class.java)
             .log("STUDENTS CALLED.")
             .zipWith(
                 webClient.get()
-                    .uri("http://localhost:8081/api/tutors")
+                    .uri(TUTORS_API)
                     .retrieve()
                     .bodyToFlux(Tutor::class.java)
                     .log("TUTORS CALLED.")
             )
+    }
+
+    companion object {
+        const val STUDENTS_API = "http://localhost:8082/api/students"
+        const val TUTORS_API = "http://localhost:8081/api/tutors"
     }
 }
